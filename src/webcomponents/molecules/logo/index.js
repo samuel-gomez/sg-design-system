59f109f1835034794aca5e13191d94af0cace21e
Samuel Gomez <contact@samuelgomez.fr>
2020-11-04 00:10:25 +0100

    feat(logo): logo web component

--- src/webcomponents/molecules/logo/index.js
+++ src/webcomponents/molecules/logo/index.js
@@ -1,14 +1,57 @@
 import style from './style.css';
 import template from './template.html';
 
+import replaceProps from '../../_helpers/replaceProps';
+
 class SgLogo extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
+    this.src = this.getAttribute('src');
+    this.title = this.getAttribute('title');
+    this.alt = this.getAttribute('alt');
+  }
+
+  static get observedAttributes() {
+    return ['src', 'title', 'alt'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'src' && oldValue !== newValue) {
+      this.updateSrc(newValue);
+    }
+    if (name === 'title' && oldValue !== newValue) {
+      this.updateTitle(newValue);
+    }
+    if (name === 'alt' && oldValue !== newValue) {
+      this.updateAlt(newValue);
+    }
+  }
+
+  updateSrc(src) {
+    this.src = src;
+    this.render();
+  }
+
+  updateTitle(title) {
+    this.title = title;
+    this.render();
+  }
+
+  updateAlt(alt) {
+    this.alt = alt;
+    this.render();
   }
 
   connectedCallback() {
-    this.shadowRoot.innerHTML = `<style>${style.toString()}</style>${template}`;
+    this.shadowRoot.innerHTML = `<style>${style.toString()}</style>${replaceProps(
+      {
+        title: this.title,
+        alt: this.alt,
+        src: this.src,
+      },
+      `${template}`,
+    )}`;
   }
 }
 
